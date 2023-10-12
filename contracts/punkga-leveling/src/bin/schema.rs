3af7521bfaae40d77e0ab3903a9b4b33e7f8e5f6
htiennv <tien.nv291997@gmail.com>
2023-10-12 14:57:12 +0700

    Initial commits.

---
+++ contracts/punkga-leveling/src/bin/schema.rs
@@ -0,0 +1,11 @@
+use cosmwasm_schema::write_api;
+
+use punkga_leveling::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+
+fn main() {
+    write_api! {
+        instantiate: InstantiateMsg,
+        execute: ExecuteMsg,
+        query: QueryMsg,
+    }
+}
